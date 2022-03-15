14da84a18d614c4d21890fcd5fb7a2068cd36733
KelvinLao-0604 <61851905+KelvinLao-0604@users.noreply.github.com>
2022-03-15 16:13:16 -0500

    Added in styles

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './App.css';
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 const CourseList = ({ courses }) => (
-  <div>
-    { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
+  <div className="course-list">
+  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
   </div>
 );
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
@@ -19,8 +20,11 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
   </div>
 );
 const schedule = {
@@ -50,10 +54,9 @@ const schedule = {
 };
 
 const App = () =>  (
-  <div>
+  <div className="container">
     <Banner title={ schedule.title } />
     <CourseList courses={ schedule.courses } />
   </div>
 );
-
 export default App;
