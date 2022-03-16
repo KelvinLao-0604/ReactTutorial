239f0c0bb18de9020ae01e45b996b141719d73bd
KelvinLao-0604 <61851905+KelvinLao-0604@users.noreply.github.com>
2022-03-15 20:52:38 -0500

    Finished Authentication

--- scheduler/src/components/Course.js
+++ scheduler/src/components/Course.js
@@ -1,5 +1,5 @@
 import {hasConflict, getCourseTerm, getCourseNumber, timeParts} from '../utilities/times.js';
-import { setData } from '../utilities/firebase.js';
+import { setData, useUserState} from '../utilities/firebase.js';
 
 const getCourseMeetingData = course => {
   const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
@@ -20,6 +20,7 @@ const reschedule = async (course, meets) => {
 };
 
 const Course = ({ course, selected, setSelected }) => {
+  const [user] = useUserState();
   const isSelected = selected.includes(course);
   const isDisabled = !isSelected && hasConflict(course, selected);
   const style = {
@@ -29,8 +30,8 @@ const Course = ({ course, selected, setSelected }) => {
   return (
     <div className="card m-1 p-2" 
         style={style}
-        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}
-        onDoubleClick={() => reschedule(course, getCourseMeetingData(course))}>
+        onClick={(isDisabled) ? null : () => setSelected(toggle(course, selected))}
+        onDoubleClick={!user ? null : () => reschedule(course, getCourseMeetingData(course))}>
       <div className="card-body">
         <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
         <div className="card-text">{ course.title }</div>
